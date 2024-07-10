355d0c3118d999179014cc3576ad18e55f648ede
bartbutenaers <bart.butenaers@gmail.com>
2024-07-10 22:00:12 +0200

    title in statestore

--- nodes/widgets/ui_notification.js
+++ nodes/widgets/ui_notification.js
@@ -8,7 +8,23 @@ module.exports = function (RED) {
         const ui = RED.nodes.getNode(config.ui)
 
         const evts = {
-            onAction: true
+            onAction: true,
+            beforeSend: function (msg) {
+                // backward compatibility for older labels via msg.payload
+                if (typeof msg.payload !== 'undefined') {
+                    statestore.set(group.getBase(), node, msg, 'title', msg.payload)
+                }
+                /**
+                 * Dynamic Properties
+                 * */
+                const updates = msg.ui_update
+                if (updates) {
+                    if (typeof (updates.title) !== 'undefined') {
+                        statestore.set(group.getBase(), node, msg, 'title', updates.title)
+                    }
+                }
+                return msg
+            }
         }
 
         // inform the dashboard UI that we are adding this node
